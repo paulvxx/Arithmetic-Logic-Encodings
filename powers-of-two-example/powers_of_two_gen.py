5bc43e0105959eb266be09ca4ec7d9d38d94f496
paulvxx <psv51037@gmail.com>
2026-08-25 22:10:09 -0400

    power of two program

--- powers-of-two-example/powers_of_two_gen.py
+++ powers-of-two-example/powers_of_two_gen.py
@@ -1,8 +1,9 @@
-import copy
-
+"""
+A cycle/sequence enumeration routine that  
+"""
 def print_powers_of_two_cycle(full_iters : int):
-    horizontal_implications = {1:3, 
-                               2:4, 
+    horizontal_implications = {1:{3}, 
+                               2:{4}, 
                                3:{5,7}, 
                                4:{6,8}, 
                                5:set(), 
@@ -14,15 +15,15 @@ def print_powers_of_two_cycle(full_iters : int):
                                11:set(),
                                12:set()
                                }
-    vertical_implications = {0:0, 
-                             1:0, 
-                             2:0, 
-                             3:2, 
-                             4:1, 
-                             5:4, 
-                             6:3, 
-                             7:4, 
-                             8:3,
+    vertical_implications = {0:{0}, 
+                             1:{0}, 
+                             2:{0}, 
+                             3:{2}, 
+                             4:{1}, 
+                             5:{4}, 
+                             6:{3}, 
+                             7:{4}, 
+                             8:{3},
                              9:set(),
                              10:set(),
                              11:set(),
@@ -41,11 +42,13 @@ def print_powers_of_two_cycle(full_iters : int):
     m2 = {9:5, 10:5, 11:5, 12:6}
     m3 = {9:7, 10:7, 11:7, 12:8}
 
+
     for _ in range(full_iters):
+        print(f"Current Sequence : {current_sequence}")
         clen = len(current_sequence)
         next_sequence = []
         for i in range(clen):
-            next_sequence[i] = m1[current_sequence[i]]
+            next_sequence.append(m1[current_sequence[i]])
 
         # read off implications
         for i in range(clen):
@@ -62,14 +65,14 @@ def print_powers_of_two_cycle(full_iters : int):
 
         next_sequence_1 = []
         next_sequence_2 = []
-        current_sequence_extended = copy.copy(current_sequence)
+        current_sequence_extended = current_sequence + current_sequence
         for i in range(clen):
-            next_sequence_1[i] = m2[current_sequence[i]]
-            next_sequence_2[i] = m3[current_sequence[i]]
+            next_sequence_1.append(m2[current_sequence[i]])
+            next_sequence_2.append(m3[current_sequence[i]])
             current_sequence_extended.append(current_sequence[i])
 
         # order doesn't matter
-        next_sequence = next_sequence_1.extend(next_sequence_2)
+        next_sequence = next_sequence_1 + next_sequence_2
 
         # read off implications
         for i in range(clen):
@@ -84,10 +87,8 @@ def print_powers_of_two_cycle(full_iters : int):
         current_sequence = next_sequence
         next_sequence = []
 
-        print(f"Current Sequence : {current_sequence}")
-        print("Current Implication List : ")
+        print(f"New Sequence : {current_sequence}")
+        print("Updated Implication List : ")
         print(f" Horizontal : {horizontal_implications} ")
         print(f" Vertical : {vertical_implications} ")
-
-
-print_powers_of_two_cycle
+        print("-----------------------------")
