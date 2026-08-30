0103e71bf2ceb62a6c7c4c33d50f569a7469caf6
paulvxx <psv51037@gmail.com>
2026-08-30 09:24:11 -0400

    done

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ Main python program to run scripts
 """
 
 from powers_of_two_example import powers_of_two_gen
-from utils import utils
+from utils import utils, fol_string_encoder
 
 h_iList, v_iList = powers_of_two_gen.print_powers_of_two_cycle(4)
 
 cnf_format = utils.implication_list_to_cnf_AEA(h_iList, v_iList)
 
-for c in cnf_format:
-    print(c)
+formula_str = fol_string_encoder.to_fol_cnf_formula(cnf_format, pretty=True)
+
+print(formula_str)
